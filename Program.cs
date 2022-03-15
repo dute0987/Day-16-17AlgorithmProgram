7c50a076ad6bb54539b823f188149835f0c41ef3
dute0987 <dute0987@gmail.com>
2022-03-15 20:25:22 +0530

    Vishal Dute- Program.cs for Merge Sort Problem

--- Program.cs
+++ Program.cs
@@ -21,9 +21,9 @@ namespace AlgorithamPrograms
                 switch (option)
                 {
                     case 1:
-                        BinarySearch binary = new BinarySearch();
-                        binary.Search();
-                        break;
+                        //BinarySearch binary = new BinarySearch();
+                        //binary.Search();
+                        //break;
 
                     //            case 2:
                     //                InsertionSort insertion = new InsertionSort();
@@ -39,10 +39,10 @@ namespace AlgorithamPrograms
                     //                BubbleSort sort = new BubbleSort();
                     //                sort.Bubble_Sort();
                     //                break;
-                    //            case 5:
-                    //                MergeSort sortMerge = new MergeSort();
-                    //                sortMerge.Merge_Sort();
-                    //                break;
+                                case 5:
+                                    MergeSort sortMerge = new MergeSort();
+                                    sortMerge.Merge_Sort();
+                                    break;
                     //            case 6:
                     //                PrimeNumbers numbers = new PrimeNumbers();
                     //                numbers.Prime();
